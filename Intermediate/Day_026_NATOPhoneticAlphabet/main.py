c0d163e78dc38bd761e24cb88977da256066c8ec
Cryo98 <alex.crispiels@gmail.com>
2023-11-16 21:20:31 +0100

    Added error handling
    A non-alphabetical character will lead to asking again an input

--- Intermediate/Day_026_NATOPhoneticAlphabet/main.py
+++ Intermediate/Day_026_NATOPhoneticAlphabet/main.py
@@ -6,6 +6,7 @@
 # [X] Convert the alphabet into a dictionary with dict comprehension
 # [X] Get user input
 # [X] Convert input into phonetic list
+# [X] Add KeyError handling for non-alphabetical input
 
 import os
 import pandas as pd
@@ -21,6 +22,13 @@ phonetic_alphabet_df = pd.read_csv(alphabet_path)
 alphabet_dict = {row["letter"]: row["code"] for (index, row) in phonetic_alphabet_df.iterrows()}
 
 # Convert user input into phonetic
-user_input = input("What word you want to spell?: ").upper()
-spelled_input = [alphabet_dict[letter] for letter in user_input]
+is_running = True
+while is_running:
+    try:
+        user_input = input("What word you want to spell?: ").upper()
+        spelled_input = [alphabet_dict[letter] for letter in user_input]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+    else:
+        is_running = False
 print(spelled_input)
