59c1f4945733efe8bd72dfb4d57c5ab79bddd8aa
Cryo98 <alex.crispiels@gmail.com>
2023-11-27 22:35:28 +0100

    Add call to sunrise/sunset API

--- Intermediate+/Day_033_ISSTracker/main.py
+++ Intermediate+/Day_033_ISSTracker/main.py
@@ -1,9 +1,26 @@
 import requests
+from datetime import datetime
 
 ISS_API_ENDPOINT = "http://api.open-notify.org/iss-now.json"
+SUN_API_ENDPOINT = "https://api.sunrise-sunset.org/json"
+LATITUDE = "45.5"
+LONGITUDE = "10"
 
-response = requests.get(url=ISS_API_ENDPOINT)
-response.raise_for_status()
+parameters = {
+    "lat": LATITUDE,
+    "lng": LONGITUDE,
+    "formatted": 0,
+}
 
-data = response.json()["iss_position"]
-print(data)
+response_sun = requests.get(url=SUN_API_ENDPOINT, params=parameters)
+response_iss = requests.get(url=ISS_API_ENDPOINT)
+response_sun.raise_for_status()
+response_iss.raise_for_status()
+
+sunrise_str = response_sun.json()["results"]["sunrise"]
+sunset_str = response_sun.json()["results"]["sunset"]
+sunrise_dt = datetime.strptime(sunrise_str, "%Y-%m-%dT%H:%M:%S%z")
+sunset_dt = datetime.strptime(sunset_str, "%Y-%m-%dT%H:%M:%S%z")
+print("Sunrise:", sunrise_dt)
+print("Sunset:", sunset_dt)
+print(sunrise_dt < sunset_dt)
