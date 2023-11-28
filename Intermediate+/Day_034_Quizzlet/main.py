a60b0f94b0919af4e7991847d1a0fd2b775321d6
Cryo98 <alex.crispiels@gmail.com>
2023-11-28 20:06:36 +0100

    Starting file

---
+++ Intermediate+/Day_034_Quizzlet/main.py
@@ -0,0 +1,14 @@
+# QUIZZLET
+# Using the "Open Trivia Databse" (<https://opentdb.com>) to create a simple
+# quiz game with GUI
+
+import requests
+import tkinter as tk
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
