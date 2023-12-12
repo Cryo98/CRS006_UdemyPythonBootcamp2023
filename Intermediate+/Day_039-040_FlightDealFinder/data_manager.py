dc9080946e7dfc928a1e42f0901abd6dcfcfe27e
Cryo98 <alex.crispiels@gmail.com>
2023-12-12 22:41:19 +0100

    Initialization of data manager

--- Intermediate+/Day_039-040_FlightDealFinder/data_manager.py
+++ Intermediate+/Day_039-040_FlightDealFinder/data_manager.py
@@ -1,3 +1,14 @@
+SHEETY_ENDPOINT = "https://api.sheety.co/"
+
+
 class DataManager:
-    #This class is responsible for talking to the Google Sheet.
-    pass
+    # This class is responsible for talking to the Google Sheet.
+    def __init__(self) -> None:
+        self.token = ""
+        self.url = ""
+
+    def set_authentication(self, token):
+        self.token = token
+
+    def set_sheet(self, url):
+        self.url = SHEETY_ENDPOINT + url
