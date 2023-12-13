de251cf4c78c2c4406c49bae719dd8a7a9f6152f
Cryo98 <alex.crispiels@gmail.com>
2023-12-13 23:26:15 +0100

    Added decorator for checking answer

--- Intermediate+/Day_039-040_FlightDealFinder/data_manager.py
+++ Intermediate+/Day_039-040_FlightDealFinder/data_manager.py
@@ -6,9 +6,23 @@ class DataManager:
     def __init__(self) -> None:
         self.token = ""
         self.url = ""
+        self.authentication_set = False
+        self.sheet_set = False
 
     def set_authentication(self, token):
         self.token = token
+        self.authentication_set = True
 
     def set_sheet(self, url):
         self.url = SHEETY_ENDPOINT + url
+        self.sheet_set = True
+
+    def check_response(self, func):
+        """Checks"""
+        def run_until(kwargs):
+            status_code = 503
+            while status_code == 503:
+                response = func(**kwargs)
+                status_code = response.status_code
+            return response
+        return run_until
