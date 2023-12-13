d8d850f443c5560cb4eb2b757193e63f0e8672ed
Cryo98 <alex.crispiels@gmail.com>
2023-12-13 23:11:38 +0100

    Added initial items for the app

--- Intermediate+/Day_037_HabitTracker/app.py
+++ Intermediate+/Day_037_HabitTracker/app.py
@@ -7,15 +7,25 @@ class HabitTracker(tk.Tk):
         super().__init__(screenName, baseName, className, useTk, sync, use)
         self.title("Habit Tracker")
         self.geometry("500x700")
+        self.graphs = [""]
+        self.graph = tk.StringVar()
+        self.__init_graph_canvas()
+        self.__init_buttons()
         self.mainloop()
 
     def __init_graph_canvas(self):
         """Initializes the canvas showing the graph"""
-        self.graph = tk.Canvas(width=400, height=200)
-        self.graph.grid(column=0, row=0)
+        self.graph_canvas = tk.Canvas(width=400, height=200)
+        self.graph_canvas.grid(column=0, row=0, columnspan=2)
+        self.habit_plot = tk.Canvas(width=400, height=100)
+        self.habit_plot.grid(column=0, row=2, columnspan=2)
 
     def __init_buttons(self):
         """Initializes the control buttons"""
+        self.add_button = tk.Button(text="Add entry")
+        self.add_button.grid(column=0, row=1)
+        self.graphs_list = tk.OptionMenu(self, self.graph, *self.graphs)
+        self.graphs_list.grid(column=1, row=1)
         pass
 
 
