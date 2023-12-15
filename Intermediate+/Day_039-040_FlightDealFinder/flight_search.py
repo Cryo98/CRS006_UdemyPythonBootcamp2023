f777e3feb987fa9cb553495402286679438131c4
Cryo98 <alex.crispiels@gmail.com>
2023-12-15 22:43:48 +0100

    Initial implementation of search functionality

--- Intermediate+/Day_039-040_FlightDealFinder/flight_search.py
+++ Intermediate+/Day_039-040_FlightDealFinder/flight_search.py
@@ -1,3 +1,23 @@
+import requests
+
+TEQUILA_ENDPOINT = "https://api.tequila.kiwi.com/"
+
+
 class FlightSearch:
-    #This class is responsible for talking to the Flight Search API.
-    pass
+    # This class is responsible for talking to the Flight Search API.
+    def __init__(self) -> None:
+        self.api_key = ""
+        self.header = ""
+
+    def set_key(self, key) -> None:
+        self.api_key = key
+        self.header = {"apikey": key}
+
+    def query(self, term) -> requests.Response:
+        params = {
+            "term": term,
+            "location_types": "city",
+            "limit": 1,
+        }
+        response = requests.get(url=TEQUILA_ENDPOINT, params=params, headers=self.header)
+        return response
--- Intermediate+/Day_039-040_FlightDealFinder/main.py
+++ Intermediate+/Day_039-040_FlightDealFinder/main.py
@@ -1,6 +1,7 @@
 # This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
 from dotenv import load_dotenv
 from data_manager import DataManager
+from flight_search import FlightSearch
 import os
 
 if __name__ == "__main__":
@@ -8,3 +9,7 @@ if __name__ == "__main__":
     data_manager = DataManager()
     data_manager.set_authentication(os.environ.get("SHEETY_TOKEN", ""))
     data_manager.set_sheet(os.environ.get("SHEETY_URL", ""))
+    # print(data_manager.get_data().json())
+    search = FlightSearch()
+    search.set_key(os.environ.get("TEQUILA_API_KEY", ""))
+    print(search.query("London").status_code)
