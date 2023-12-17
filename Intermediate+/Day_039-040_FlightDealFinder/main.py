f3661a734f1f8a401563ad53286d7c41ad46e9df
Cryo98 <alex.crispiels@gmail.com>
2023-12-17 21:09:12 +0100

    Added loop to update IATA codes

--- Intermediate+/Day_039-040_FlightDealFinder/main.py
+++ Intermediate+/Day_039-040_FlightDealFinder/main.py
@@ -4,13 +4,18 @@ from data_manager import DataManager
 from flight_search import FlightSearch
 import os
 
+
 if __name__ == "__main__":
     load_dotenv()
     data_manager = DataManager()
     data_manager.set_authentication(os.environ.get("SHEETY_TOKEN", ""))
     data_manager.set_sheet(os.environ.get("SHEETY_URL", ""))
-    # print(data_manager.get_data().json())
+    data = data_manager.get_data().json()["prices"]
+
     search = FlightSearch()
     search.set_key(os.environ.get("TEQUILA_API_KEY", ""))
-    print(search.query("London").json()["locations"][0]["code"])
-    print(search.query("Paris").json()["locations"][0]["code"])
+    for city_data, idx in zip(data, range(len(data))):
+        iata_code = search.get_city_code(city_data["city"])
+        print(data_manager.set_data(idx+1, "iataCode", iata_code).json())
+    # print(search.query("London").json()["locations"][0]["code"])
+    # print(search.query("Paris").json()["locations"][0]["code"])
