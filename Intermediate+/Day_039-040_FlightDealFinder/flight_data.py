cca8f8b38a782ee60898b37f07bc6fe0349c7555
Cryo98 <alex.crispiels@gmail.com>
2023-12-19 22:39:51 +0100

    Moved file-specific functions to FlightData class

--- Intermediate+/Day_039-040_FlightDealFinder/flight_data.py
+++ Intermediate+/Day_039-040_FlightDealFinder/flight_data.py
@@ -1,3 +1,16 @@
-class FlightData:
-    #This class is responsible for structuring the flight data.
-    pass
+from data_manager import DataManager
+
+
+class FlightData(DataManager):
+    # This class is responsible for structuring the flight data.
+    def __init__(self) -> None:
+        super().__init__()
+
+    def set_iata_code(self, code: str, idx: int) -> bool:
+        body = {
+            "prices": {
+                "iataCode": code
+            }
+        }
+        response = self.set_data(row=idx+2, body=body)
+        return response.status_code == 200
