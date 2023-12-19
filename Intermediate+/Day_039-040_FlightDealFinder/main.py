7d6fc193105042f52f17e5b1f8e53e884cdae7b2
Cryo98 <alex.crispiels@gmail.com>
2023-12-19 22:41:06 +0100

    DataManager -> FlightData

--- Intermediate+/Day_039-040_FlightDealFinder/main.py
+++ Intermediate+/Day_039-040_FlightDealFinder/main.py
@@ -1,6 +1,7 @@
-# This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+# This file will need to use the DataManager,FlightSearch, FlightData,
+# NotificationManager classes to achieve the program requirements.
 from dotenv import load_dotenv
-from data_manager import DataManager
+from flight_data import FlightData
 from flight_search import FlightSearch
 import os
 
@@ -10,20 +11,65 @@ TOKENLESS = True
 
 if __name__ == "__main__":
     load_dotenv()
-    data_manager = DataManager()
+    data_manager = FlightData()
     data_manager.set_authentication(os.environ.get("SHEETY_TOKEN", ""))
     data_manager.set_sheet(os.environ.get("SHEETY_URL", ""))
     if TOKENLESS:
         data = [
-            {'city': 'Paris', 'iataCode': 'PAR', 'lowestPrice': 54, 'id': 2},
-            {'city': 'Berlin', 'iataCode': 'BER', 'lowestPrice': 42, 'id': 3},
-            {'city': 'Tokyo', 'iataCode': 'TYO', 'lowestPrice': 485, 'id': 4},
-            {'city': 'Sydney', 'iataCode': 'SYD', 'lowestPrice': 551, 'id': 5},
-            {'city': 'Istanbul', 'iataCode': 'IST', 'lowestPrice': 95, 'id': 6},
-            {'city': 'Kuala Lumpur', 'iataCode': 'KUL', 'lowestPrice': 414, 'id': 7},
-            {'city': 'New York', 'iataCode': 'NYC', 'lowestPrice': 240, 'id': 8},
-            {'city': 'San Francisco', 'iataCode': 'SFO', 'lowestPrice': 260, 'id': 9},
-            {'city': 'Cape Town', 'iataCode': 'CPT', 'lowestPrice': 378, 'id': 10}
+            {
+                'city': 'Paris',
+                'iataCode': 'PAR',
+                'lowestPrice': 54,
+                'id': 2
+                },
+            {
+                'city': 'Berlin',
+                'iataCode': 'BER',
+                'lowestPrice': 42,
+                'id': 3
+                },
+            {
+                'city': 'Tokyo',
+                'iataCode': 'TYO',
+                'lowestPrice': 485,
+                'id': 4
+                },
+            {
+                'city': 'Sydney',
+                'iataCode': 'SYD',
+                'lowestPrice': 551,
+                'id': 5
+                },
+            {
+                'city': 'Istanbul',
+                'iataCode': 'IST',
+                'lowestPrice': 95,
+                'id': 6
+                },
+            {
+                'city': 'Kuala Lumpur',
+                'iataCode': 'KUL',
+                'lowestPrice': 414,
+                'id': 7
+                },
+            {
+                'city': 'New York',
+                'iataCode': 'NYC',
+                'lowestPrice': 240,
+                'id': 8
+                },
+            {
+                'city': 'San Francisco',
+                'iataCode': 'SFO',
+                'lowestPrice': 260,
+                'id': 9
+                },
+            {
+                'city': 'Cape Town',
+                'iataCode': 'CPT',
+                'lowestPrice': 378,
+                'id': 10
+                }
             ]
     else:
         data = data_manager.get_data().json()["prices"]
@@ -37,4 +83,4 @@ if __name__ == "__main__":
             iata_code = search.get_city_code(city_data["city"])
             data[idx]["iataCode"] = iata_code
             if not TOKENLESS:
-                data_manager.set_data(idx+2, "iataCode", iata_code)
+                data_manager.set_iata_code(code=iata_code, idx=idx)
