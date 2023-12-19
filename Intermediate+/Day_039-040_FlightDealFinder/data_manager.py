0d44106f87feb5b314ee5df2cae2ed88fb13d906
Cryo98 <alex.crispiels@gmail.com>
2023-12-19 22:40:08 +0100

    Fixed response check decorator

--- Intermediate+/Day_039-040_FlightDealFinder/data_manager.py
+++ Intermediate+/Day_039-040_FlightDealFinder/data_manager.py
@@ -11,44 +11,44 @@ class DataManager:
         self.authentication_set = False
         self.sheet_set = False
 
-    def set_authentication(self, token):
+    def set_authentication(self, token) -> None:
         self.token = token
         self.header = {"Authorization": f"Bearer {token}"}
         self.authentication_set = True
 
-    def set_sheet(self, url):
+    def set_sheet(self, url) -> None:
         self.url = SHEETY_ENDPOINT + url
         self.sheet_set = True
 
-    def get_data(self) -> requests.Response:
-        response = requests.get(self.url, headers=self.header)
-        return response
-
-    def set_data(self, row: int, column: str, data) -> requests.Response:
-        header = self.header
-        header["Content-Type"] = "application/json"
-        body = {
-            "price": {
-                column: data,
-            }
-        }
-        response = requests.put(self.url + f"/{row}", json=body, headers=header)
-        return response
-
-    def check_response(self, func):
+    @staticmethod
+    def check_response(func):
         """Checks the response from a HTTP request and handles errors."""
-        def request_checked(kwargs):
+        def wrapper(self, *args, **kwargs) -> requests.Response | None:
             response: requests.Response
             if not self.authentication_set or not self.sheet_set:
                 print("No valid authentication and sheet have been chosen.")
                 print("Please set them using 'set_authentication' and 'set_sheet'.")
                 return None
+            response: requests.Response = func(self, *args, **kwargs)
             try:
-                response = func(**kwargs)
                 response.raise_for_status()
-            except requests.HTTPError:
+            except requests.HTTPError as error:
                 if response.status_code == 401:
                     print("Invalid authorization, please insert the correct token.")
+                else:
+                    print(f"HTTP Error occurred: {error}")
             finally:
                 return response
-        return request_checked
+        return wrapper
+
+    @check_response
+    def get_data(self) -> requests.Response:
+        response = requests.get(self.url, headers=self.header)
+        return response
+
+    @check_response
+    def set_data(self, row: int, body: dict) -> requests.Response:
+        header = self.header
+        header["Content-Type"] = "application/json"
+        response = requests.put(self.url + f"/{row}", json=body, headers=header)
+        return response
